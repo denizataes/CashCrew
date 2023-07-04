c701890848ce62825924cd3872ec2930e304137b
Deniz Ata EŞ <85442526+denizataes@users.noreply.github.com>
2023-07-04 20:08:58 +0300

    Initial Commit

---
+++ CashCrew/CashCrewApp.swift
@@ -0,0 +1,17 @@
+//
+//  CashCrewApp.swift
+//  CashCrew
+//
+//  Created by Deniz Ata Eş on 4.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct CashCrewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
